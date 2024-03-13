1c4b5d3ad8d92fcea38a272dca2a2268576be536
alepazro <117799413+alepazro@users.noreply.github.com>
2024-03-13 11:29:21 -0500

    commit

--- etapp/etapp/Scripts/buyNowButton.js
+++ etapp/etapp/Scripts/buyNowButton.js
@@ -2,7 +2,7 @@
 function buyNow() {
     try {
         if (userIsAdmin == true) {
-            window.open('https://easitrack.net/buy-now.html?s=1&t=' + getTokenCookie('ETTK'), target = "_blank");
+            window.open('https://easitrack.com/buy-now.html?s=1&t=' + getTokenCookie('ETTK'), target = "_blank");
         }
         else {
             alert('This option is only available for Administrator users');
